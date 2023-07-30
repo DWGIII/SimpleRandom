3e1096ed0af6125c3a5658aa5d70df1ad65a5fd6
MyGen <93558630+DWGIII@users.noreply.github.com>
2023-07-30 08:58:01 +0800

    Create [Win] 随机30位字母.bat

---
+++ [Win] 随机30位字母.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+set "string=abcdefghijklmnopqrstuvwxyz"
+set "result="
+for /L %%i in (1,1,30) do call :add
+echo %result%
+pause
+goto :eof
+
+:add
+set /a x=%random% %% 26 
+set result=%result%!string:~%x%,1!
+goto :eof
